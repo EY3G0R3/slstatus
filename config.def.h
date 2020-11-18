c7a8cc00ff1738142810221714b7247778e84dac
igorg <igorg@fb.com>
2020-11-17 17:09:10 -0800

    volume: removed : between output and volume now that we are showing icons instead

--- config.def.h
+++ config.def.h
@@ -82,7 +82,7 @@ static const struct arg args[] = {
   // igorg: try without volume in status; pa-applet should be enough
   //{ vol_perc            , "%s", "/dev/mixer" },
   { run_command         , " / %s", "~/bin/igoraudio_status"},
-  { run_command         , ": %s", "~/.config/i3blocks/blocks/volume"},
+  { run_command         , " %s", "~/.config/i3blocks/blocks/volume"},
   //{ run_command       , " %s", "~/.config/i3blocks/blocks/volume-pulseaudio"},
   { datetime            , " / %s", "%Y-%m-%d" },
   // battery script prints a single space when no battery is installed
